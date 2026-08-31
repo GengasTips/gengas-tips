30fd1cae1dcff62255b86ad3431266e69b554e56
GengasTips <firestickhomies@gmail.com>
2026-08-31 15:22:15 +0800

    Update tagline to reflect new focus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Genga's Tips",
-  tagline: 'Tips to simplify your life.',
+  tagline: 'Tips to remove subscriptions from your life.',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
