c05c0d3d0d20ab90931b2398c594f1863608bc93
GengasTips <firestickhomies@gmail.com>
2026-09-02 13:09:14 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
@@ -28,9 +26,6 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
